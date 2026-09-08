e1d024922682c043cb1c6acab6a7dbefed332710
aiswaryarajeev12345 <aiswaryarajeevpr104@gmail.com>
2026-09-08 21:11:38 +0530

    Configure static files for Render

--- project1/settings.py
+++ project1/settings.py
@@ -44,13 +44,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+  
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'project1.urls'
@@ -123,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
